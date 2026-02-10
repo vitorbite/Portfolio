c7fb9665fe503582cbb78fe9fec9bd9a4c30e14c
vitorbite <vitorbitencourt20@gmail.com>
2026-02-10 19:16:23 -0300

    Criando uma base para o Portfolio

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+export default function index() {
+  return (
+    <>
+      <footer>
+        <ol className="flex flex-row">
+          <li className="p-2">
+            <a href="https://github.com/vitorbite">GitHub</a>
+          </li>
+          <li className="p-2">
+            <a href="#">LinkedIn</a>
+          </li>
+          <li className="p-2">
+            <a href="#">email@gmail.com</a>
+          </li>
+        </ol>
+      </footer>
+    </>
+  );
+}
