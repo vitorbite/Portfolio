8dceb0597e86391bae170c9b29eba958f07d52c9
vitorbite <vitorbitencourt20@gmail.com>
2026-02-10 20:09:55 -0300

    Criando um Menu basico

--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -1,7 +1,13 @@
+import styles from "./style.module.css";
+
 export default function index() {
   return (
-    <div className="back">
-
+    <div className="min-w-screen h-15 border-b-amber-50 border-b bg-blue-950 flex justify-center items-center">
+      <div className="flex justify-between max-w-1/2 w-2xl min-w-100 h-15 items-center">
+        <button className={styles.button}>Home</button>
+        <button className={styles.button}>Sobre Mim</button>
+        <button className={styles.button} id={styles.Projetos}>Projetos</button>
+      </div>
     </div>
-  )
+  );
 }
