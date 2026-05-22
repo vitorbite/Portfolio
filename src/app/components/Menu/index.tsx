353c8360560f624d624d07d8817c68e0163a7af2
vitorbite <vitorbitencourt20@gmail.com>
2026-05-21 23:21:03 -0300

    Removendo nome da nav

--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -7,7 +7,7 @@ export default function index() {
     <nav className="fixed top-0 z-50">
       <div className="min-w-screen h-15 border-b-amber-50 border-b bg-blue-950 flex justify-center items-center">
         <div className="flex justify-between max-w-1/2 w-2xl min-w-100 h-15 items-center">
-        <h2>Vitorbite</h2>
+        {/* <h2>Vitorbite</h2> */}
             <button className={styles.button} onClick={() => Scroll("Home")}>Home</button>
             <button className={styles.button} onClick={() => Scroll("Sobre")} id="SobreBtn">Sobre</button>
             <button className={styles.button} onClick={() => Scroll("Projetos")} id={styles.ProjetosBtn}>
